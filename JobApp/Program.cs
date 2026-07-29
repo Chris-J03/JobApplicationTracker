0d963a7045c24bbeb4fa11df20cc00498348f438
Chris <edward_chris03@outlook.com>
2026-07-29 11:54:16 +0100

    Initial push, framework for menu

---
+++ JobApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace JobApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Call the Run method to begin the program's execution and display the menu options to the user
+            Menu.Run();   
+        }
+    }
+}
