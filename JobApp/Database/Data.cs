0b671527230b803996ef1b2d8c23a12c5c18390a
Chris <edward_chris03@outlook.com>
2026-08-04 17:17:24 +0100

    Updated Data folder if no table

--- JobApp/Database/Data.cs
+++ JobApp/Database/Data.cs
@@ -6,11 +6,32 @@ using Microsoft.Data.Sqlite;
 
 public class Database
 {
-    private readonly string connectionString =
-        "Data Source=jobs.db";
+    private readonly string connectionString = "Data Source=jobs.db";
 
     public SqliteConnection GetConnection()
     {
         return new SqliteConnection(connectionString);
+    }
+
+    public void Initialise()
+    {
+        using SqliteConnection connection = GetConnection();
+
+        connection.Open();
+
+        string sql =
+        @"
+        CREATE TABLE IF NOT EXISTS Jobs
+        (
+            Id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Company TEXT NOT NULL,
+            Position TEXT NOT NULL,
+            Status TEXT NOT NULL
+        );
+        ";
+
+        using SqliteCommand command = new SqliteCommand(sql, connection);
+
+        command.ExecuteNonQuery();
     }
 }
