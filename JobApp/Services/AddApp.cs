25456855d29109c24dd16199a2b51017ba6f7f4a
Chris <edward_chris03@outlook.com>
2026-08-06 22:38:49 +0100

    Beginning for adding applications

--- JobApp/Services/AddApp.cs
+++ JobApp/Services/AddApp.cs
@@ -9,6 +9,16 @@ namespace JobApp
             // Logic to add application goes here
             // Make this step asynchronous to prevent user waiting for the operation to complete
             // For example, you can use Task.Run to run the database operation in a separate thread
+            //Company, position, and status are required fields for adding a new job application
+            Console.WriteLine("Please enter the company name:");
+            string? company = Console.ReadLine();
+            Console.WriteLine("Please enter the position:");
+            string? position = Console.ReadLine();
+            Console.WriteLine("Please enter the status:");
+            string? status = Console.ReadLine();
         }
+        
+        
+    
     }
 }
