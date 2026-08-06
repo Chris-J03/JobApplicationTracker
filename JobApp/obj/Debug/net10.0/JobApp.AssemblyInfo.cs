e87fdb01d0a330c379df65422d02e17aec2ccb04
Chris <edward_chris03@outlook.com>
2026-08-06 12:23:29 +0100

    Beginning viewing of apps

--- JobApp/obj/Debug/net10.0/JobApp.AssemblyInfo.cs
+++ JobApp/obj/Debug/net10.0/JobApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4372ef2be482a38ca4616280494335feb8e4f69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0da4a0c56a088eaa5b38862145f83b0968c65f1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
