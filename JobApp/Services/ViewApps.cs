2fb9d053570665f26471404b07516f47e78acc9a
Chris <edward_chris03@outlook.com>
2026-08-06 12:50:49 +0100

    Updated logic for viewing application

---
+++ JobApp/Services/ViewApps.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
+namespace JobApp
+{
+    public class ViewApps
+    {
+        // Method to view all job applications in the database
+        public void ViewApplications()
+        {
+            Console.WriteLine("Viewing all applications...");
+
+            // Logic to retrieve and display applications goes here
+            // This can be a synchronous operation since it is just reading data
+
+            using SqliteConnection connection = new SqliteConnection("Data Source=Database/Jobs.db");
+            connection.Open();
+            
+            string sql = @"
+                SELECT Id, Company, Position, Status
+                FROM Jobs;";
+
+            using SqliteCommand command = new SqliteCommand(sql, connection);
+            using SqliteDataReader reader = command.ExecuteReader();
+
+            List<JobApp.Model.Job> jobs = new();
+
+            while (reader.Read())
+            {
+                jobs.Add(new JobApp.Model.Job
+                {
+                    Id = reader.GetInt32(0),
+                    JobName = reader.GetString(1),
+                    Company = reader.GetString(2),
+                    Status = reader.GetString(3)
+                });
+            }
+        }
+    }
+}
--- JobApp/obj/Debug/net10.0/JobApp.AssemblyInfo.cs
+++ JobApp/obj/Debug/net10.0/JobApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1645e09745158f5a99610ff5e4f9bd9fb8dc947a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+def4c34ec4de37865b6b3ca56181d73dd127ef4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
