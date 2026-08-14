a3d99b1bf708debea05d4ea1eca4c5a60953f8a7
Chris <edward_chris03@outlook.com>
2026-08-14 11:33:13 +0100

    Imap removed as will be using Microsoft Graph

--- JobApp/Services/EmailReader.cs
+++ JobApp/Services/EmailReader.cs
@@ -16,11 +16,7 @@ namespace JobApp
             // Logic to read emails goes here
             // For example, you can use MailKit to connect to the user's email account and read the emails
             // You can then extract the relevant information from the email content and update the database with new job applications
-            using (var client = new ImapClient())
-            {
-                client.Connect("imap.example.com", 993, true);
-                client.Authenticate("username", "password");
-            }
+
         }
     }
 }
--- JobApp/obj/Debug/net10.0/JobApp.AssemblyInfo.cs
+++ JobApp/obj/Debug/net10.0/JobApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52125b3e9f4cc54073c9a3d9ebc7cc9e73028910")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c3f999f42bfeb9d390b0cccfe11657d07320193")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
